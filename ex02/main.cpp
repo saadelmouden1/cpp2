ed12f17f14a6d50930af7c7bd5b6c0e576c7a86a
saad elmoudden <sel-moud@c1r8p3.1337.ma>
2025-05-01 22:51:12 +0100

    l

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -9,10 +9,10 @@ static std::string	boolToStr(bool b)
 
 static void	testComparison(void)
 {
+	std::cout<<"*****************Comparison Test********************"<<std::endl;
 	Fixed	a(4.95f);
 	Fixed	b(5);
 
-	std::cout<<"*****************Comparison Test********************"<<std::endl;
 	std::cout << "a = " << a << "  b = " << b << std::endl;
 	std::cout << "  a > b ? " << boolToStr(a > b) << std::endl;
 	std::cout << "  a < b ? " << boolToStr(a < b) << std::endl;
@@ -39,10 +39,10 @@ static void	testComparison(void)
 
 static void	arithmeticTesting(void)
 {
+	std::cout<<"*****************Aretmethic Test************************"<<std::endl;
 	Fixed	a(4.95f);
 	Fixed	b(5);
 
-	std::cout<<"*****************Aretmethic Test************************"<<std::endl;
 	std::cout << "a = " << a << "  b = " << b << std::endl;
 	std::cout << "  a + b = " << a + b << std::endl;
 	std::cout << "  a + a = " << a + a << std::endl;
@@ -53,11 +53,11 @@ static void	arithmeticTesting(void)
 
 static void	maxMinTesting(void)
 {
+	std::cout<<"****************MAX/MIN Test*****************"<<std::endl;
 	Fixed		a(-5.25f);
 	Fixed		b(-2);
 	Fixed const	c(-3);
 
-	std::cout<<"****************MAX/MIN Test*****************"<<std::endl;
 	std::cout << "a = " << a << "  b = " << b << std::endl;
 	std::cout << "  max(a,b) = " << Fixed::max(a, b) << std::endl;
 	std::cout << "  min(a,b) = " << Fixed::min(a, b) << std::endl;
@@ -68,10 +68,10 @@ static void	maxMinTesting(void)
 
 static void	subjectTesting(void)
 {
+	std::cout<<"**************subject test*****************"<<std::endl;
 	Fixed		a;
 	Fixed const	b(Fixed(5.05f) * Fixed(2));
 
-	std::cout<<"**************subject test*****************"<<std::endl;
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
